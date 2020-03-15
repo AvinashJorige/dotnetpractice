755d37eccc5b72fe88c62d476bbd67799a9f285b
Avinash Jorige <jo.avi.1990@gmail.com>
2020-03-15 23:04:02 +0530

    Connection and data repository user validation

---
+++ DataRepository/Repo1.cs
@@ -0,0 +1,42 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace DataRepository
+{
+    public class Repo1
+    {
+        private readonly SqlConnection _conn = null;
+        public Repo1()
+        {
+            this._conn = new SqlConnection("Data Source=DESKTOP-13LL2HE\\SQLSERVER;Initial Catalog=Practice;Persist Security Info=True;User ID=sa;Password=jorige");
+        }
+
+        public bool ValidateUserLogin(string UserId, string Password)
+        {
+            DataTable dt = new DataTable();
+            bool flag = false;
+
+            if(!string.IsNullOrEmpty(UserId) && !string.IsNullOrEmpty(Password))
+            {
+                return flag;
+            }           
+
+            SqlCommand sqlCommand = new SqlCommand(@"select isnull(COUNT(id),0) from tbl_Emp_Password where EmpId in (
+                                                    select EmpId from Practice..Tbl_Emp_Master_M where LeavingDate is null and IsIILEmp = 1 and Status = 1)
+                                                    and EmpId = '" + UserId + "' and Password = '" + Password +"'");
+            _conn.Open();
+            SqlDataReader sqlData = sqlCommand.ExecuteReader();
+            dt.Load(sqlData);
+
+            if(dt != null && dt.Rows.Count > 0)
+            {
+                if(dt.Rows[0][0].ToString() == "0")
+                {
+                    return flag;
+                }                
+            }
+
+            return true;
+        }
+    }
+}
